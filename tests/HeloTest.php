58b151c8dee6b61f5e9c5ff9cd36cc193e09a44a
Joel Butcher <joelbutcher@users.noreply.github.com>
2023-02-23 14:21:04 +0000

    Use pest as test framework

--- tests/HeloTest.php
+++ tests/HeloTest.php
@@ -2,52 +2,38 @@
 
 namespace BeyondCode\HeloLaravel\Tests;
 
-use BeyondCode\HeloLaravel\HeloLaravelServiceProvider;
 use BeyondCode\HeloLaravel\Mailer;
 use BeyondCode\HeloLaravel\Laravel7Mailer;
 use BeyondCode\HeloLaravel\TestMail;
 use BeyondCode\HeloLaravel\TestMailCommand;
 use Illuminate\Support\Facades\Mail;
-use Orchestra\Testbench\TestCase;
 
-class HeloTest extends TestCase
+uses(TestCase::class);
+
+test('the mail commands sends the mailable', function ()
 {
-    protected function getPackageProviders($app)
-    {
-        return [
-            HeloLaravelServiceProvider::class
-        ];
+    Mail::fake();
+
+    $this->artisan(TestMailCommand::class);
+
+    Mail::assertSent(TestMail::class);
+});
+
+test('plain text mails work correctly(', function ()
+{
+    Mail::fake();
+
+    Mail::to('test@usehelo.com')->send(new TestMail(true));
+
+    Mail::assertSent(TestMail::class);
+});
+
+test('the correct mailer is binded', function () {
+    $mailer = app(Mailer::class);
+
+    if (version_compare(app()->version(), '7.0.0', '<')) {
+        $this->assertTrue($mailer instanceof Mailer);
+    } else {
+        $this->assertTrue($mailer instanceof Laravel7Mailer);
     }
-
-    /** @test */
-    public function test_the_mail_commands_sends_the_mailable()
-    {
-        Mail::fake();
-
-        $this->artisan(TestMailCommand::class);
-
-        Mail::assertSent(TestMail::class);
-    }
-
-    /** @test */
-    public function test_plain_text_mails_work_correctly()
-    {
-        Mail::fake();
-
-        Mail::to('test@usehelo.com')->send(new TestMail(true));
-
-        Mail::assertSent(TestMail::class);
-    }
-
-    /** @test */
-    public function test_the_correct_mailer_is_binded()
-    {
-        $mailer = app(Mailer::class);
-
-        if (version_compare(app()->version(), '7.0.0', '<')) {
-            $this->assertTrue($mailer instanceof Mailer);
-        } else {
-            $this->assertTrue($mailer instanceof Laravel7Mailer);
-        }
-    }
-}
+});
